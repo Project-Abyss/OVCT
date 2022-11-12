7a0cd66d1ae312df529d90d6d0cfa1c03c9e9ca7
xkeBANg <115547189+xkeBANg@users.noreply.github.com>
2022-11-12 23:49:13 +0800

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -1,2 +1,8 @@
+import pandas
+
 def load_table():
-    pass
+    PubVPN_URL = "http://www.vpngate.net/api/iphone/"
+    Source = pandas.read_csv(PubVPN_URL, header=1)
+    Source = Source[['#HostName', 'CountryLong', 'IP', 'Speed', 'OpenVPN_ConfigData_Base64']]
+    print(Source.head(10)) # List the first 10 rows of the PublicVPN list
+    print('\n---------------------\n')

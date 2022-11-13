0f55f0781328300dc78816d7c648a0201537ac86
Nima-Huang <87530200+Nima-Huang@users.noreply.github.com>
2022-11-13 14:34:51 +0800

    Update vpnselection.py

--- vpnselection.py
+++ vpnselection.py
@@ -1,2 +1,15 @@
+import pandas 
+import csv
+
 def select_one(filtered_result):
-    pass
+
+    list_file = pandas.read_csv(filtered_result)
+    vpn_hostname = input("Please input VPN ISP hostname: \n\n=> ")
+
+    while(vpn_hostname not in list(list_file['#HostName'])):
+
+        vpn_hostname = input("Please input VPN ISP hostname: \n\n=> ")
+        print("\n[Sorry, this Hostname isn't in the PublicVPN_list.csv, please input again.]\n")
+    
+    print('\n---------------------\n')
+    return vpn_hostname  

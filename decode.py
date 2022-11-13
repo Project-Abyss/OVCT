c569129d9c16d3402bf132eb127b3dc89cc7961d
Nima-Huang <87530200+Nima-Huang@users.noreply.github.com>
2022-11-13 14:31:33 +0800

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,2 +1,13 @@
+import base64
+
 def vpn(vpn_hostname):
-    pass
+
+    fliter = list_file['#HostName'] == vpn_hostname
+    VPN_data = list_file[(fliter)]
+
+    base64_message = str(VPN_data['OpenVPN_ConfigData_Base64'].values)
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    ovpn_file_path = message_bytes.decode('ascii')
+
+    return ovpn_file_path

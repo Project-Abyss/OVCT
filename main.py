1f59b938f4c8a54fbbfe03c3d7a783e9d6003535
Nima-Huang <87530200+Nima-Huang@users.noreply.github.com>
2022-11-18 10:42:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
         elif function_chioce == '3':
             print('\n-----------------------------------\n')
-            filtered_csv_path = input("Where are your vpn data list: \n\n=>")
+            filtered_csv_path = input("【 Please enter your VPN list path: 】\n\n=>")
             vpn_hostname, vpn_ip, vpn_country = vpnselection.select_one(filtered_csv_path)
             ovpn_file_content = decode.vpn(filtered_csv_path, vpn_hostname)
 

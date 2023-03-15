140c62bca7affdf6d1c26bfe920ee2f62cc6a602
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-15 16:44:37 +0800

    Update load_module.py

--- load_module.py
+++ load_module.py
@@ -10,7 +10,8 @@ def loading():
             continue
         modile_name = file_path.stem
         module_path = str(file_path.relative_to(functions_path)).replace("/", ".")[:-3]
-        module_spec = importlib.util.module_from_spec(module_spec)
+        module_spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(module_spec)
         module_spec.loader.exec_module(module)
         
         globals()[module_name] = module

d440a4d5adffbcbfdfc01f00a9e9b8c0c7ba3548
Chiao52 <88649220+Chiao52@users.noreply.github.com>
2023-03-16 09:05:22 +0800

    Update ovct.py

--- ovct.py
+++ ovct.py
@@ -1,13 +1,11 @@
 import os, sys
 import configparser as cp
-inifile_name = "./config.ini"
-inifile = cp.ConfigParser()
-inifile.read(inifile_name, 'UTF-8')
-
 from import_functions import *
 
 if __name__ == "__main__":
     loading()
-    
+    inifile_name = "./config.ini"
+    inifile = cp.ConfigParser()
+    inifile.read(inifile_name, 'UTF-8')
     Updated_Source = initialize.get_current_list(inifile["File_Path"]["all_resources"])
     menu.choose_operation(Updated_Source, inifile["File_Path"]["all_resources"], inifile["File_Path"]["resources"])

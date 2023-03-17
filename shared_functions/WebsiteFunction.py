f551ac9833bdcd4bcff8cdde0806d822664d9ca9
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-18 03:08:01 +0800

    Update and rename import_functions.py to shared_functions/WebsiteFunction.py

---
+++ shared_functions/WebsiteFunction.py
@@ -0,0 +1,13 @@
+from shared_functions import DataProcessing
+from respective_websites.vpngate.vpngate_filter import VpnFilter
+from respective_websites.vpngate.vpngate_connection import VpnSelection, Connection
+
+class Vpngate:
+	def update(File_Path):
+		DataProcessing.appendNewList(File_Path)
+	def filter(Folder_Path, File_Name):
+		VpnFilter.ChooseFilterWay.filter(Folder_Path, File_Name)
+	def connection(Source, Filtered_Csv_Path):
+		vpn_hostname, vpn_ip, vpn_country = VpnSelection.selectOne(Source, Show_List = "y")
+		ovpn_file_content = VpnSelection.decodeSelectedVpn(Filtered_Csv_Path, vpn_hostname)
+		Connection.systemIdentify(vpn_hostname, vpn_ip, vpn_country, ovpn_file_content)

34e9403b2b61227d287cc0a4aca185e1c186b19c
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-18 03:15:45 +0800

    Update ovct.py

--- ovct.py
+++ ovct.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     inifile_name = "./config.ini"
     inifile = cp.ConfigParser()
     inifile.read(inifile_name, 'UTF-8')
-    Menu.chooseOperation(inifile["File_Path"]["resources"], inifile["File_Path"]["vpngate_resources_name"], inifile["website_info"]["website_name"])
+    Menu.chooseOperation(inifile["file_path"]["resources"], inifile["file_path"]["vpngate_resources_name"], inifile["website_info"]["website_name"])

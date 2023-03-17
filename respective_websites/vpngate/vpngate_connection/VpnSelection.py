ce9cfe79acae058a67b7c7ebf08a1ff52b25de49
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-18 03:02:59 +0800

    Update and rename functions/connection/vpnselection.py to respective_websites/vpngate/vpngate_connection/VpnSelection.py

--- respective_websites/vpngate/vpngate_connection/VpnSelection.py
+++ respective_websites/vpngate/vpngate_connection/VpnSelection.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import csv, base64
 
-def select_one(Source, show_list):
+def selectOne(Source, Show_List):
     list_file = Source
-    if show_list == "y":
+    if Show_List == "y":
         print('\n-----------------------------------\n\n【 Public VPN 10 filtered records 】\n')
         print(list_file[['#HostName', 'CountryLong', 'IP', 'Speed']].head(10))
         print('\n-----------------------------------\n')
@@ -20,21 +20,21 @@ def select_one(Source, show_list):
     print('\n-----------------------------------\n')
 
     filter = list_file['#HostName'] == vpn_hostname
-    VPN_data = list_file[(filter)].values.tolist()[0]
-    vpn_ip = VPN_data[2]
-    vpn_country = VPN_data[1]
+    vpn_data = list_file[(filter)].values.tolist()[0]
+    vpn_ip = vpn_data[2]
+    vpn_country = vpn_data[1]
     if " " in vpn_country:
         vpn_country = vpn_country.replace(" ", "_")
     else:
         pass
     return vpn_hostname, vpn_ip, vpn_country
 
-def decode_selected_vpn(filtered_csv_path, vpn_hostname):
-    list_file = pd.read_csv(filtered_csv_path)
-    filter = list_file['#HostName'] == vpn_hostname
-    VPN_data = list_file[(filter)]
+def decodeSelectedVpn(Filtered_Csv_Path, Vpn_Hostname):
+    list_file = pd.read_csv(Filtered_Csv_Path)
+    filter = list_file['#HostName'] == Vpn_Hostname
+    vpn_data = list_file[(filter)]
 
-    base64_message = str(VPN_data['OpenVPN_ConfigData_Base64'].values)
+    base64_message = str(vpn_data['OpenVPN_ConfigData_Base64'].values)
     base64_bytes = base64_message.encode('ascii')
     message_bytes = base64.b64decode(base64_bytes)
     ovpn_file_content = message_bytes.decode('ascii')

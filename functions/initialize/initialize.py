ca44bf6faa8c110586e1f1549848f204fb25aad9
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-18 03:05:11 +0800

    Update and rename functions/initialize/initialize.py to respective_websites/vpngate/vpngate_initialize/Initialize.py

--- functions/initialize/initialize.py
+++
@@ -1,8 +0,0 @@
-from functions import data_processing
-from functions.initialize import webcrawler
-
-def get_current_list(All_Source_Path):
-    print("\n[ Initializing... ]")
-    Updated_Source = webcrawler.load_table()
-    data_processing.list_reload(Updated_Source, All_Source_Path)
-    return Updated_Source

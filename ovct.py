37c151b39e8d16cf90babeabf878ff9702ebea19
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-18 02:50:31 +0800

    Update ovct.py

--- ovct.py
+++ ovct.py
@@ -1,11 +1,9 @@
 import os, sys
 import configparser as cp
-from import_functions import *
+from shared_functions import Menu
 
 if __name__ == "__main__":
-    loading()
     inifile_name = "./config.ini"
     inifile = cp.ConfigParser()
     inifile.read(inifile_name, 'UTF-8')
-    Updated_Source = initialize.get_current_list(inifile["File_Path"]["all_resources"])
-    menu.choose_operation(Updated_Source, inifile["File_Path"]["all_resources"], inifile["File_Path"]["resources"])
+    Menu.chooseOperation(inifile["File_Path"]["resources"], inifile["File_Path"]["vpngate_resources_name"], inifile["website_info"]["website_name"])
